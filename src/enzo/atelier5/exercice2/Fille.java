3c10d56becd98a3ef9bcb3f64260a31943d44969
EnzoZa <71082476+EnzoZa@users.noreply.github.com>
2020-11-11 18:35:13 +0100

    first commit exercice2 td5

---
+++ src/enzo/atelier5/exercice2/Fille.java
@@ -0,0 +1,10 @@
+package enzo.atelier5.exercice2;
+
+public class Fille extends Mere{
+    private Object unObjetSensible;
+
+    public Fille(int unI, UneClasse unObj, Object unObjSens){
+        super(unI,unObj);
+        unObjetSensible = unObjSens;
+    }
+}
---
+++ src/enzo/atelier5/exercice2/Mere.java
@@ -0,0 +1,11 @@
+package enzo.atelier5.exercice2;
+
+public class Mere{
+    private int i;
+    private UneClasse unObjet; //Clonable
+
+    public Mere(int unI, UneClasse unObj){
+        i = unI;
+        unObjet = unObj;
+    }
+} 
